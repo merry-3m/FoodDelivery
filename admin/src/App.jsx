f691d3d0e0b0ae6fe959bdd0e8fc60ae0483f4fb
merry-3m <149013303+merry-3m@users.noreply.github.com>
2024-09-19 19:11:52 -0500

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   // ` backend url
-  const url = "http://localhost:3000";
+  const url = "https://food-delivery-backend-dpw6.onrender.com";
   return (
     <>
     <ToastContainer />
